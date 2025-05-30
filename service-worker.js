3a0f215aff77a284fecfd72630c3282ad9559da7
Yash <tempmail.dustyop@gmail.com>
2025-05-31 01:33:19 +0530

    hole-master uploaded

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'hole-collector-cache-v1';
+const urlsToCache = [
+  './', // Caches the root, i.e., index.html
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com', // Caches Tailwind CSS from CDN
+  // Add paths to any other assets like images, sounds, fonts if you add them later
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
